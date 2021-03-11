b30b196d9ff653c6cc2c554f95ab0538d4cee577
Harun-or-rashid <harun35-2093@diu.edu.bd>
2021-03-12 02:52:35 +0600

    social login issue fixed

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -44,7 +44,32 @@ class AuthController extends Controller
     public function githubRedirect()
     {
         $user=Socialite::driver('github')->user();
-        dd($user);
+        $data=([
+            'email'=>$user->email,
+            'password'=>$user->nickname,
+        ]);
+        if (!Auth()->attempt($data))
+        $users=User::firstOrCreate([
+            'email'=>$user->email,
+            'name'=>$user->nickname,
+            'image'=>Str::random(24),
+            'phone'=>$user->id,
+            'role'=>1,
+            'password'=>bcrypt($user->nickname)
+        ]);
+        if (Auth()->attempt($data)){
+            return redirect('/events/create');
+        }
+
+//        dd($data);
+        elseif (Auth()->attempt($data))
+        {
+            return redirect('/events/create');
+        }
+        else{
+
+            return redirect()->back();
+        }
     }
     public function facebook()
     {
@@ -53,7 +78,33 @@ class AuthController extends Controller
     public function facebookRedirect()
     {
         $user=Socialite::driver('facebook')->user();
-        dd($user);
+
+        $data=([
+            'email'=>$user->email,
+            'password'=>$user->id,
+        ]);
+
+if (!auth()->attempt($data)){
+        $users=User::firstOrCreate([
+            'email'=>$user->email,
+            'name'=>$user->name,
+            'image'=>Str::random(24),
+            'phone'=>$user->id,
+            'role'=>1,
+            'password'=>bcrypt($user->id)
+        ]);
+        if (auth()->attempt($data))
+        {
+            return redirect('/events/create');
+        }
+    }elseif (auth()->attempt($data))
+        {
+            return redirect('/events/create');
+        }
+        else{
+
+            return redirect()->back();
+        }
     }
 
 
